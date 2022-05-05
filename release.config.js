fce929236e3b851f02c8d4b84567f2f9dae4a4f0
Marco Lovazzano <87663863+marcolovazzano@users.noreply.github.com>
2022-05-05 09:10:37 +0200

    feat(release): add config

---
+++ release.config.js
@@ -0,0 +1,44 @@
+// https://semantic-release.gitbook.io/semantic-release/
+
+module.exports = {
+  'branches': [
+    'main',
+    {
+      'name': 'develop',
+      'channel': 'channel-${name}',
+      'prerelease': true
+    },
+  ],
+  'repositoryUrl': 'https://github.com/marcolovazzano/monorepo-playground',
+  'tagFormat': 'v${version}',
+  'preset': 'conventionalcommits',
+  'plugins': [
+    ['@semantic-release/commit-analyzer', {
+      }
+    ],
+    ['@semantic-release/release-notes-generator', {
+        'writerOpts': {
+          'commitsSort': ['perf', 'feat', 'fix'],
+        }
+      }
+    ],
+    ['@semantic-release/exec', {
+        'verifyReleaseCmd': 'echo ${nextRelease.version} > .VERSION',
+      }
+    ],
+    ['@semantic-release/changelog', {
+        'changelogFile': 'CHANGELOG.md',
+        'changelogTitle': '# Release Changelog'
+      }
+    ],
+    ['@semantic-release/git', {
+        'assets': ['CHANGELOG.md']
+      }
+    ],
+    ['@semantic-release/github', {
+      'assets': [
+        {'path': 'CHANGELOG.md'},
+      ]
+    }],
+  ]
+}
