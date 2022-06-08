85b4ff02a8df478cf0651e1b7d34e672bb87bf46
Marco Lovazzano <87663863+marcolovazzano@users.noreply.github.com>
2022-06-08 09:02:19 +0200

    chore(commitlint): add custom rules config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,12 @@
 // https://commitlint.js.org/
 
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  // https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional
+  extends: ['@commitlint/config-conventional'],
+  /*
+    * Any rules defined here will override rules from @commitlint/config-conventional
+    */
+  rules: {
+  }
 };
 
